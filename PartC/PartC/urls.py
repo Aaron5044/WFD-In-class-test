ede3584d543acf684dc9336ae75a6bf7c7940190
Aaron5044 <B00158358@mytudublin.ie>
2025-03-24 13:54:02 +0000

    commit 2
    commit 2

--- PartC/PartC/urls.py
+++ PartC/PartC/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyApp.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # This makes "home" the default page
 ]
